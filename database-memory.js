6fc79bdca66f8ec04ff0a436157985df1286e656
Lucas Soares <lucas.ssoares.oliveiira@gmail.com>
2023-09-08 17:38:17 -0300

    deploy application

--- database-memory.js
+++ database-memory.js
@@ -3,9 +3,24 @@ import { randomUUID } from "crypto"
 export class DataBaseMemory {
     #videos = new Map()
 
-    list(){
-        return this.#videos.values()
-    } 
+    list(search){
+        return Array.from( this.#videos.entries()).map((videoArray) => {
+              const id = videoArray[0]
+              const data = videoArray[1]
+
+              return {
+                id,
+                ...data,
+              }
+        })
+        .filter(video => {
+            if(search) {
+                return video.title.includes(search)
+            }
+
+            return true
+        })
+    }
 
     create(video){
         const videoId = randomUUID()
--- server.js
+++ server.js
@@ -1,35 +1,58 @@
 import { fastify } from "fastify";
-import { DataBaseMemory } from './database-memory.js'
+import { DataBasePostgres } from "./database-postgres.js";
 
 
 const server = fastify()
 
-const database = new DataBaseMemory()
+const database = new DataBasePostgres()
 
-server.post('/videos', (req, reply) => {
-    database.create({
-        title: 'Video 01',
-        descripton: 'Esse é o video 01',
-        duration: 100,
+server.post('/videos', async (req, reply) => {
+     const {title, description, duration} = req.body
+
+    
+
+ await database.create({
+        title,
+        description,
+        duration,
     })
     
-    console.log(database.list())
+   
 
     return reply.status(201).send()
 })
 
-server.get('/videos', () => {
-    
+server.get('/videos', async (req) => { 
+    const search = req.query.search
+
+    console.log(search)
+
+    const videos = await database.list(search)
+
+    return videos
 })
 
-server.put('/videos/:id', () => {
-    
+server.put('/videos/:id', async (req, reply) => {
+    const videoId = req.params.id
+    const {title, description, duration} = req.body
+
+   await database.update(videoId, {
+        title,
+        description,
+        duration,
+    })
+
+    return reply.status(204).send()
 })
 
-server.delete('/videos/:id', () => {
-    
+server.delete('/videos/:id', async (req, reply) => {
+    const videoId = req.params.id
+
+   await database.delete(videoId)
+
+    return reply.status(204).send()
 })
 
 server.listen({
-    port:3333,
+    port: process.env.PORT ?? 3333,
 })
